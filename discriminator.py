9885f19c14c5a64bb523f7a690036a22a8f415e7
Kajetan <kajetanozog@gmail.com>
2024-06-01 16:02:06 +0200

    Improved discriminator

--- discriminator.py
+++ discriminator.py
@@ -20,19 +20,19 @@ def build_discriminator():
     conv2d_1 = tf.keras.layers.BatchNormalization(momentum=0.99)(conv2d_1)
     conv2d_1 = tf.keras.layers.LeakyReLU(alpha=0.3)(conv2d_1)
 
-   # conv2d_2 = tf.keras.layers.Conv2D(128, kernel_size=3, strides=2, padding='same')(conv2d_1)
-   # conv2d_2 = tf.keras.layers.BatchNormalization()(conv2d_2)
-    #conv2d_2 = tf.keras.layers.LeakyReLU(alpha=0.2)(conv2d_2)
+    conv2d_2 = tf.keras.layers.Conv2D(128, kernel_size=3, strides=2, padding='same')(conv2d_1)
+    conv2d_2 = tf.keras.layers.BatchNormalization()(conv2d_2)
+    conv2d_2 = tf.keras.layers.LeakyReLU(alpha=0.2)(conv2d_2)
 
-   # conv2d_3 = tf.keras.layers.Conv2D(256, kernel_size=3, strides=2, padding='same')(conv2d_2)
-   # conv2d_3 = tf.keras.layers.BatchNormalization()(conv2d_3)
-   # conv2d_3 = tf.keras.layers.LeakyReLU(alpha=0.2)(conv2d_3)
+    conv2d_3 = tf.keras.layers.Conv2D(256, kernel_size=3, strides=2, padding='same')(conv2d_2)
+    conv2d_3 = tf.keras.layers.BatchNormalization()(conv2d_3)
+    conv2d_3 = tf.keras.layers.LeakyReLU(alpha=0.2)(conv2d_3)
 
    # conv2d_4 = tf.keras.layers.Conv2D(512, kernel_size=3, strides=2, padding='same')(conv2d_3)
     #conv2d_4 = tf.keras.layers.BatchNormalization()(conv2d_4)
    # conv2d_4 = tf.keras.layers.LeakyReLU(alpha=0.2)(conv2d_4)
 
-    flatten = tf.keras.layers.Flatten()(conv2d_1)
+    flatten = tf.keras.layers.Flatten()(conv2d_3)
     dense_1 = tf.keras.layers.Dense(1, activation='sigmoid')(flatten)
 
     model = tf.keras.Model(inputs=[image_input, label_input], outputs=dense_1)
