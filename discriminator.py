7a9789dfb7e644874521c7edb06e17b310c027cd
Oskar Kulinski <oskarkulinski3@gmail.com>
2024-06-01 15:49:15 +0200

    Reworked calculating loss, and gradient applying

--- discriminator.py
+++ discriminator.py
@@ -17,22 +17,22 @@ def build_discriminator():
     concatenated = tf.keras.layers.Concatenate()([image_input, label_embedding])
 
     conv2d_1 = tf.keras.layers.Conv2D(64, kernel_size=3, strides=2, padding='same')(concatenated)
-    conv2d_1 = tf.keras.layers.BatchNormalization()(conv2d_1)
-    conv2d_1 = tf.keras.layers.LeakyReLU(alpha=0.2)(conv2d_1)
+    conv2d_1 = tf.keras.layers.BatchNormalization(momentum=0.99)(conv2d_1)
+    conv2d_1 = tf.keras.layers.LeakyReLU(alpha=0.3)(conv2d_1)
 
-    conv2d_2 = tf.keras.layers.Conv2D(128, kernel_size=3, strides=2, padding='same')(conv2d_1)
-    conv2d_2 = tf.keras.layers.BatchNormalization()(conv2d_2)
-    conv2d_2 = tf.keras.layers.LeakyReLU(alpha=0.2)(conv2d_2)
+   # conv2d_2 = tf.keras.layers.Conv2D(128, kernel_size=3, strides=2, padding='same')(conv2d_1)
+   # conv2d_2 = tf.keras.layers.BatchNormalization()(conv2d_2)
+    #conv2d_2 = tf.keras.layers.LeakyReLU(alpha=0.2)(conv2d_2)
 
-    conv2d_3 = tf.keras.layers.Conv2D(256, kernel_size=3, strides=2, padding='same')(conv2d_2)
-    conv2d_3 = tf.keras.layers.BatchNormalization()(conv2d_3)
-    conv2d_3 = tf.keras.layers.LeakyReLU(alpha=0.2)(conv2d_3)
+   # conv2d_3 = tf.keras.layers.Conv2D(256, kernel_size=3, strides=2, padding='same')(conv2d_2)
+   # conv2d_3 = tf.keras.layers.BatchNormalization()(conv2d_3)
+   # conv2d_3 = tf.keras.layers.LeakyReLU(alpha=0.2)(conv2d_3)
 
-    conv2d_4 = tf.keras.layers.Conv2D(512, kernel_size=3, strides=2, padding='same')(conv2d_3)
-    conv2d_4 = tf.keras.layers.BatchNormalization()(conv2d_4)
-    conv2d_4 = tf.keras.layers.LeakyReLU(alpha=0.2)(conv2d_4)
+   # conv2d_4 = tf.keras.layers.Conv2D(512, kernel_size=3, strides=2, padding='same')(conv2d_3)
+    #conv2d_4 = tf.keras.layers.BatchNormalization()(conv2d_4)
+   # conv2d_4 = tf.keras.layers.LeakyReLU(alpha=0.2)(conv2d_4)
 
-    flatten = tf.keras.layers.Flatten()(conv2d_4)
+    flatten = tf.keras.layers.Flatten()(conv2d_1)
     dense_1 = tf.keras.layers.Dense(1, activation='sigmoid')(flatten)
 
     model = tf.keras.Model(inputs=[image_input, label_input], outputs=dense_1)
--- model.py
+++ model.py
@@ -12,23 +12,23 @@ import logging
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 
+tf.random.set_seed(42)
+
 
 class SceneGenerator:
     def __init__(self):
         self.discriminator = build_discriminator()
         self.generator = build_generator()
 
-        discriminator_optimizer = tf.keras.optimizers.Adam(1.5e-4,0.5)
-        self.discriminator.compile(loss='binary_crossentropy', optimizer=discriminator_optimizer, metrics=['accuracy'])
+        self.generator_optimizer = tf.keras.optimizers.Adam(1.5e-4,0.5)
+        self.discriminator_optimizer = tf.keras.optimizers.Adam(1.5e-4,0.5)
 
-        self.discriminator.trainable = False
         noise_input = tf.keras.layers.Input(shape=(params.noise_dim,))
         label_input = tf.keras.layers.Input(shape=(params.num_classes,))
 
         generated_image = self.generator([noise_input, label_input])
         validity = self.discriminator([generated_image, label_input])
         self.gan = tf.keras.Model([noise_input, label_input], validity)
-        self.gan.compile(loss='binary_crossentropy', optimizer='adam')
 
     def generate_noise(self, batch_size, noise_dim):
         return np.random.normal(0, 1, (batch_size, noise_dim))
@@ -40,10 +40,16 @@ class SceneGenerator:
 
         return one_hot_labels
 
-    def train(self, train_dataset, epochs, batch_size, sample_interval, save_interval=10):
-        valid = np.ones((batch_size, 1))
-        fake = np.zeros((batch_size, 1))
+    def discriminator_loss(self, real_output, fake_output):
+        real_loss = tf.keras.losses.binary_crossentropy(tf.ones_like(real_output), real_output)
+        fake_loss = tf.keras.losses.binary_crossentropy(tf.zeros_like(fake_output), fake_output)
+        total_loss = real_loss + fake_loss
+        return total_loss
 
+    def generator_loss(self, fake_output):
+        return tf.keras.losses.binary_crossentropy(tf.ones_like(fake_output), fake_output)
+
+    def train(self, train_dataset, epochs, batch_size, sample_interval, save_interval=10):
         current_datetime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
         folder_name = os.path.join("saved_models", current_datetime)
         os.makedirs(folder_name, exist_ok=True)
@@ -52,46 +58,49 @@ class SceneGenerator:
         g_loss = 0
 
         for epoch in range(epochs):
+            gen_loss_list = []
+            disc_loss_list = []
+
+            # training step on one batch
             for real_images, real_labels in train_dataset:
-                # ---------------------
-                #  Train Discriminator
-                # ---------------------
 
-                self.discriminator.trainable = True
+                with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+                    batch_size = real_images.shape[0]
 
-                batch_size = real_images.shape[0]
-                if batch_size != valid.shape[0]:
-                    valid = np.ones((batch_size, 1))
-                    fake = np.zeros((batch_size, 1))
+                    # Generate a batch of fake images
+                    noise = self.generate_noise(batch_size, params.noise_dim)
+                    fake_labels = self.generate_labels(batch_size, params.num_classes)
+                    #fake_images = self.generator.predict([noise, fake_labels])
 
-                # Generate a batch of fake images
-                noise = self.generate_noise(batch_size, params.noise_dim)
-                fake_labels = self.generate_labels(batch_size, params.num_classes)
-                fake_images = self.generator.predict([noise, fake_labels])
-                #print(fake_labels)
+                    fake_images = self.generator([noise, fake_labels], training=True)
 
-                # Train the discriminator
-                d_loss_real = self.discriminator.train_on_batch([real_images, real_labels], valid)
-                d_loss_fake = self.discriminator.train_on_batch([fake_images, fake_labels], fake)
-                d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+                    real_output = self.discriminator([real_images, real_labels], training=True)
+                    fake_output = self.discriminator([fake_images, fake_labels], training=True)
 
-                # ---------------------
-                #  Train Generator
-                # ---------------------
+                    gen_loss = self.generator_loss(fake_output)
+                    disc_loss = self.discriminator_loss(real_output, fake_output)
 
-                self.discriminator.trainable = False
+                    gradients_of_generator = gen_tape.gradient(gen_loss,
+                                                               self.generator.trainable_variables)
+                    gradients_of_discriminator = disc_tape.gradient(disc_loss,
+                                                                    self.discriminator.trainable_variables)
 
-                # Generate a batch of noise and labels
-                noise = self.generate_noise(batch_size, params.noise_dim)
-                sampled_labels = self.generate_labels(batch_size, params.num_classes)
+                    self.generator_optimizer.apply_gradients(zip(
+                        gradients_of_generator, self.generator.trainable_variables))
+                    self.discriminator_optimizer.apply_gradients(zip(
+                        gradients_of_discriminator,
+                        self.discriminator.trainable_variables))
 
-                # Train the generator (to have the discriminator label samples as valid)
-                g_loss = self.gan.train_on_batch([noise, sampled_labels], valid)
+            # calculating total loss in the epoch
+            gen_loss_list.append(sum(gen_loss.numpy()))
+            disc_loss_list.append(sum(disc_loss.numpy()))
 
-            print(f"{epoch} [D loss: {d_loss[0]}, acc.: {100 * d_loss[1]:.2f}%] [G loss: {g_loss}]")
+            g_loss: float = sum(gen_loss_list) / len(gen_loss_list)
+            d_loss: float = sum(disc_loss_list) / len(disc_loss_list)
+            print(f"{epoch}: [D loss: {d_loss}, [G loss: {g_loss}]")
 
             # If at save interval, save generated image samples
-            if epoch % (sample_interval//5) == 0:
+            if epoch % (sample_interval // 5) == 0:
                 self.sample_images(epoch)
 
             if epoch != 0 and epoch % save_interval == 0:
@@ -123,7 +132,12 @@ class SceneGenerator:
 
         fig, axs = plt.subplots(1, 5, figsize=(5, 2))
         for i in range(params.num_classes):
-            axs[i].imshow(gen_images[i])
-            axs[i].set_title(f"{sampled_labels[i]}")
+            index = 0
+            for j in range(5):
+                if sampled_labels[j][i] == 1:
+                    index = j
+                    break
+            axs[i].imshow((gen_images[i] * 255).astype(np.uint8))
+            axs[i].set_title(f"{index}")
             axs[i].axis('off')
         plt.show()
