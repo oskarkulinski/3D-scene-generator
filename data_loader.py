ef87ace12801e5f46efe11d3725343d9aa87c702
Oskar Kulinski <oskarkulinski3@gmail.com>
2024-06-02 14:21:26 +0200

    Changed rescaling back to [-1,1]

--- data_loader.py
+++ data_loader.py
@@ -37,7 +37,7 @@ class DataLoader:
                         # Load and preprocess the image
                         image = tf.keras.utils.load_img(image_path, target_size=(params.image_height, params.image_width))
                         image_array = tf.keras.utils.img_to_array(image)
-                        image_array = (image_array / 255) # Normalize to [-1, 1]
+                        image_array = (image_array / 127.5) - 1 # Normalize to [-1, 1]
                         image_list.append(image_array)
 
                         label_list.append(class_names.index(class_name))
