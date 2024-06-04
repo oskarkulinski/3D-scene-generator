2fbce3d982fe1d8557ba5cce8fac60bf1eabd6d1
Oskar Kulinski <oskarkulinski3@gmail.com>
2024-06-04 20:42:16 +0200

    Removed dense layer from generator, replaced upsampling with convolution with stride 2, removed dropout, changed beta_1 in optimizer to 0.5 in accordance with recommendations "Unsupervised Representation Learning with Deep Convolutional Generative Adversarial Networks" by Alec Radford, Luke Metz, Soumith Chintala, 2015

--- generator.py
+++ generator.py
@@ -10,38 +10,28 @@ def build_generator():
     label_input = tf.keras.Input(shape=params.num_classes, name='label_input')
 
     label_embedding = tf.keras.layers.Embedding(params.num_classes, 5)(label_input)
+    label_embedding = tf.keras.layers.Dense(92, activation='relu')(label_embedding)
     label_embedding = tf.keras.layers.Flatten()(label_embedding)
 
     concatenated_input = tf.keras.layers.Concatenate()([noise_input, label_embedding])
 
-    dense_1 = tf.keras.layers.Dense(128 * 8 * 8, activation='relu')(concatenated_input)
-    normal_1 = tf.keras.layers.BatchNormalization(momentum=0.8)(dense_1)
-    reshape = tf.keras.layers.Reshape((8, 8, 128))(normal_1)
+    reshape = tf.keras.layers.Reshape((8, 8, 3))(concatenated_input)
 
-    upsample_1 = tf.keras.layers.UpSampling2D(size=(2, 2))(reshape)  # 16x16
-    conv_1 = tf.keras.layers.Conv2DTranspose(256, kernel_size=3, padding='same', activation='relu')(upsample_1)
-    drop_1 = tf.keras.layers.Dropout(0.25)(conv_1)
-    normal_2 = tf.keras.layers.BatchNormalization(momentum=0.8)(drop_1)
+    conv_1 = tf.keras.layers.Conv2DTranspose(256, kernel_size=3, strides=2,
+                                             padding='same', activation='relu')(reshape)
+    normal_2 = tf.keras.layers.BatchNormalization(momentum=0.8)(conv_1)
 
-    upsample_2 = tf.keras.layers.UpSampling2D(size=(2, 2))(normal_2)  # 32x32
-    conv_2 = tf.keras.layers.Conv2DTranspose(256, kernel_size=3, padding='same', activation='relu')(upsample_2)
-    drop_2 = tf.keras.layers.Dropout(0.25)(conv_2)
-    normal_3 = tf.keras.layers.BatchNormalization(momentum=0.8)(drop_2)
+    conv_2 = tf.keras.layers.Conv2DTranspose(256, kernel_size=3, strides=2,
+                                             padding='same', activation='relu')(normal_2)
+    normal_3 = tf.keras.layers.BatchNormalization(momentum=0.8)(conv_2)
 
-    upsample_3 = tf.keras.layers.UpSampling2D(size=(2, 2))(normal_3)  # 64x64
-    conv_3 = tf.keras.layers.Conv2DTranspose(128, kernel_size=3, padding='same', activation='relu')(upsample_3)
-    drop_3 = tf.keras.layers.Dropout(0.25)(conv_3)
-    normal_4 = tf.keras.layers.BatchNormalization(momentum=0.8)(drop_3)
+    conv_3 = tf.keras.layers.Conv2DTranspose(128, kernel_size=3, strides=2,
+                                             padding='same', activation='relu')(normal_3)
+    normal_4 = tf.keras.layers.BatchNormalization(momentum=0.8)(conv_3)
 
-    upsample_4 = tf.keras.layers.UpSampling2D(size=(2, 2))(normal_3)  # 64x64
-    conv_4 = tf.keras.layers.Conv2DTranspose(128, kernel_size=3, padding='same', activation='relu')(upsample_4)
-    drop_4 = tf.keras.layers.Dropout(0.25)(conv_4)
-    normal_5 = tf.keras.layers.BatchNormalization(momentum=0.8)(drop_4)
-
-    upsample_5 = tf.keras.layers.UpSampling2D(size=(2, 2))(normal_5)  # 128x128
-    conv_4 = tf.keras.layers.Conv2DTranspose(128, kernel_size=3, padding='same', activation='relu')(upsample_5)
-    drop_4 = tf.keras.layers.Dropout(0.25)(conv_4)
-    normal_5 = tf.keras.layers.BatchNormalization(momentum=0.8)(drop_4)
+    conv_4 = tf.keras.layers.Conv2DTranspose(128, kernel_size=3, strides=2,
+                                             padding='same', activation='relu')(normal_4)
+    normal_5 = tf.keras.layers.BatchNormalization(momentum=0.8)(conv_4)
 
     output = tf.keras.layers.Conv2D(3, kernel_size=3, padding='same', activation='tanh')(normal_5)
 
