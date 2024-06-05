5f8599e78d44b4873da77cc39069726886c4f625
Oskar Kulinski <oskarkulinski3@gmail.com>
2024-06-05 22:37:03 +0200

    Added a dense layer behind noise, doesn't improve anything

--- generator.py
+++ generator.py
@@ -9,22 +9,23 @@ def build_generator():
     noise_input = tf.keras.Input(shape=(params.noise_dim,), name='noise_input')
     label_input = tf.keras.Input(shape=params.num_classes, name='label_input')
     # num_classes * output_dim + noise_dim = reshape total size
-    label_embedding = tf.keras.layers.Embedding(params.num_classes, 44)(label_input)
-    label_embedding = tf.keras.layers.Flatten()(label_embedding)
+    noise_reshape = tf.keras.layers.Dense(32448, activation='relu')(noise_input)
+    noise_reshape = tf.keras.layers.Reshape((8, 8, 507))(noise_reshape)
 
-    concatenated_input = tf.keras.layers.Concatenate()([noise_input, label_embedding])
+    label_embedding = tf.keras.layers.Embedding(params.num_classes, 64)(label_input)
+    label_embedding = tf.keras.layers.Reshape((8, 8, 5))(label_embedding)
 
-    reshape = tf.keras.layers.Reshape((8, 8, 5))(concatenated_input)
+    concatenated_input = tf.keras.layers.Concatenate()([noise_reshape, label_embedding])
 
-    conv_1 = tf.keras.layers.Conv2DTranspose(512, kernel_size=3, strides=2,
-                                             padding='same', activation='relu')(reshape)
+    conv_1 = tf.keras.layers.Conv2DTranspose(256, kernel_size=3, strides=2,
+                                             padding='same', activation='relu')(concatenated_input)
     normal_2 = tf.keras.layers.BatchNormalization(momentum=0.8)(conv_1)
 
-    conv_2 = tf.keras.layers.Conv2DTranspose(256, kernel_size=3, strides=2,
+    conv_2 = tf.keras.layers.Conv2DTranspose(128, kernel_size=3, strides=2,
                                              padding='same', activation='relu')(normal_2)
     normal_3 = tf.keras.layers.BatchNormalization(momentum=0.8)(conv_2)
 
-    conv_3 = tf.keras.layers.Conv2DTranspose(128, kernel_size=3, strides=2,
+    conv_3 = tf.keras.layers.Conv2DTranspose(64, kernel_size=3, strides=2,
                                              padding='same', activation='relu')(normal_3)
     normal_4 = tf.keras.layers.BatchNormalization(momentum=0.8)(conv_3)
 
