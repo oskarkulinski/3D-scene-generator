86f850cb2d7cf7a3a1ebaf5390d3818d1c3a25b7
Oskar Kulinski <oskarkulinski3@gmail.com>
2024-06-08 21:37:52 +0200

    Decreased amount of filters, neurons in the first layer, increased batch size to 64

--- generator.py
+++ generator.py
@@ -9,8 +9,8 @@ def build_generator():
     noise_input = tf.keras.Input(shape=(params.noise_dim,), name='noise_input')
     label_input = tf.keras.Input(shape=params.num_classes, name='label_input')
 
-    noise_reshape = tf.keras.layers.Dense(32448, activation='relu')(noise_input)
-    noise_reshape = tf.keras.layers.Reshape((8, 8, 507))(noise_reshape)
+    noise_reshape = tf.keras.layers.Dense(22 * 64, activation='relu')(noise_input)
+    noise_reshape = tf.keras.layers.Reshape((8, 8, 22))(noise_reshape)
 
     label_embedding = tf.keras.layers.Embedding(params.num_classes, 64)(label_input)
     label_embedding = tf.keras.layers.Reshape((8,8,params.num_classes))(label_embedding)
@@ -18,25 +18,25 @@ def build_generator():
     concatenated_input = tf.keras.layers.Concatenate()([noise_reshape, label_embedding])
 
     upsample_1 = tf.keras.layers.UpSampling2D(size=(2, 2))(concatenated_input)  # 16x16
-    conv_1 = tf.keras.layers.Conv2DTranspose(512, kernel_size=3, padding='same')(upsample_1)
+    conv_1 = tf.keras.layers.Conv2DTranspose(256, kernel_size=3, padding='same')(upsample_1)
     dropout_1 = tf.keras.layers.Dropout(0.25)(conv_1)
     normal_2 = tf.keras.layers.BatchNormalization(momentum=0.8)(dropout_1)
     activation_1 = tf.keras.layers.ReLU()(normal_2)
 
     upsample_2 = tf.keras.layers.UpSampling2D(size=(2, 2))(activation_1)  # 32x32
-    conv_2 = tf.keras.layers.Conv2DTranspose(256, kernel_size=3, padding='same')(upsample_2)
+    conv_2 = tf.keras.layers.Conv2DTranspose(128, kernel_size=3, padding='same')(upsample_2)
     dropout_2 = tf.keras.layers.Dropout(0.25)(conv_2)
     normal_3 = tf.keras.layers.BatchNormalization(momentum=0.8)(dropout_2)
     activation_2 = tf.keras.layers.ReLU()(normal_3)
 
     upsample_3 = tf.keras.layers.UpSampling2D(size=(2, 2))(activation_2)  # 64x64
-    conv_3 = tf.keras.layers.Conv2DTranspose(128, kernel_size=3, padding='same')(upsample_3)
+    conv_3 = tf.keras.layers.Conv2DTranspose(64, kernel_size=3, padding='same')(upsample_3)
     dropout_3 = tf.keras.layers.Dropout(0.25)(conv_3)
     normal_4 = tf.keras.layers.BatchNormalization(momentum=0.8)(dropout_3)
     activation_3 = tf.keras.layers.ReLU()(normal_4)
 
     upsample_5 = tf.keras.layers.UpSampling2D(size=(2, 2))(activation_3)  # 128x128
-    conv_4 = tf.keras.layers.Conv2DTranspose(64, kernel_size=3, padding='same')(upsample_5)
+    conv_4 = tf.keras.layers.Conv2DTranspose(32, kernel_size=3, padding='same')(upsample_5)
     dropout_4 = tf.keras.layers.Dropout(0.25)(conv_4)
     normal_5 = tf.keras.layers.BatchNormalization(momentum=0.8)(dropout_4)
     activation_4 = tf.keras.layers.ReLU()(normal_5)
--- parameters.py
+++ parameters.py
@@ -1,7 +1,7 @@
 num_classes = 10
 image_height = 128
 image_width = 128
-batch_size = 16
+batch_size = 64
 epochs = 1000
 sample_interval = 25
 noise_dim = 100
