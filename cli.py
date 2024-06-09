865ec6031a5c3346311d1ba73866af3eec784af5
Oskar Kulinski <oskarkulinski3@gmail.com>
2024-06-09 23:11:00 +0200

    Fixed cli

--- cli.py
+++ cli.py
@@ -1,38 +1,23 @@
-import os
 from model import SceneGenerator
 from data_loader import DataLoader
 import parameters as params
-import numpy as np
 
 print("Do you wish to train the model or load an existing one?")
 print("1 = Train the model, 2 = Load an existing model")
 train_choice = input()
 model = SceneGenerator()
 if train_choice == "1":
-    train, test = DataLoader.get_train_test()
+    train, test = DataLoader.get_data()
 
-    model.train(train, params.epochs, params.batch_size, params.sample_interval)
+    model.train(train, params.epochs)
 
 else:
     model.load_models("main_model")
 
-
-print("Choose your class")
-classes = []
-i = 0
-for cls in os.listdir("./selected_data"):
-    classes.append(cls)
-    print("{cls} --> {i}".format(cls=cls, i=i))
-    i += 1
 while True:
-    number = input()
-    if number in range(0, len(classes)):
-        noise = np.random.normal(0, 1, 5)
-        labels = np.zeros(shape=(params.num_classes, 5))
-        for i in range(5):
-            labels[number][i] = 1
-        model.generator.predict([noise, labels])()
-    else:
+    decision = input("If you want to quit type q, otherwise type g")
+    if decision == "q":
         break
+    else:
+        model.sample_images()
 
-# TODO add showing of pictures, add endline when choosing class, add incrementing of i
